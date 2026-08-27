a05a04fa2c64576991f09cfc0125e4e47897ec52
Arju Paul <arjupaul2006@gmail.com>
2026-08-28 00:56:20 +0530

    Batch Prediction Completed

--- app.py
+++ app.py
@@ -12,14 +12,19 @@ from Network_Security_System.logger import logging
 from Network_Security_System.constants.training_pipeline import DATA_INGESTIN_DATABASE_NAME, DATA_INGESTIN_COLLECTION_NAME
 
 from Network_Security_System.pipeline.training_pipeline import TrainingPipeline
+from Network_Security_System.utils.ml_utils.model.estimator import NetworkModel
+from form_data import FormData
+from typing import Annotated
 
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, File, UploadFile, Request
+from fastapi import FastAPI, File, UploadFile, Request, Form
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 
 from Network_Security_System.utils.main_utils.utils import load_obj
+
+
 
 
 from dotenv import load_dotenv
@@ -43,6 +48,10 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+# import the HTML File
+from fastapi.templating import Jinja2Templates
+template = Jinja2Templates(directory='./templates')
+
 @app.get('/', tags=['authentication'])
 async def index():
     return RedirectResponse(url='/docs')
@@ -53,6 +62,73 @@ async def train_route():
         training_pipeline = TrainingPipeline()
         training_pipeline.run_pipeline()
         return Response('Training is successful')
+
+    except Exception as e:
+        raise CustomeException(e, sys)
+
+
+
+@app.post('/predict')
+async def predict(request: Request, file:UploadFile = File(...)):
+    try:
+        preprocessor = load_obj('final_model/preprocessor.pkl')
+        model = load_obj('final_model/model.pkl')
+
+        network_model = NetworkModel(preprocessor=preprocessor, model=model)
+
+        df = pd.read_csv('valid_data/test.csv')
+        y_pred = network_model.predict(df)
+        df['Predicted_output'] = y_pred
+
+        df.to_csv('predicted_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+
+        return template.TemplateResponse(
+            request=request,
+            name="table.html",
+            context={"table": table_html}
+        )
+
+    except Exception as e:
+        raise CustomeException(e, sys)
+
+
+@app.get('/form')
+async def form_page(request: Request):
+    try:
+        return template.TemplateResponse(
+            request=request,
+            name='form.html',
+            context={}
+        )
+
+    except Exception as e:
+        raise CustomeException(e, sys)
+
+
+@app.post('/form')
+async def form_prediction(
+    request: Request,
+    form_data: Annotated[FormData, Form()]
+):
+    try:
+        print(form_data)
+
+        preprocessor = load_obj('final_model/preprocessor.pkl')
+        model = load_obj('final_model/model.pkl')
+        network_model = NetworkModel(preprocessor=preprocessor, model=model)
+        df = pd.DataFrame([form_data.model_dump()])
+        prediction = network_model.predict(df)
+
+        prediction = -1.0 if prediction == 0.0 else 1.0
+
+        print('Predicted result: ', prediction)
+
+        return template.TemplateResponse(
+            request=request,
+            name='form.html',
+            context={'result': prediction}
+        )
 
     except Exception as e:
         raise CustomeException(e, sys)
---
+++ form_data.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+
+class FormData(BaseModel):
+    having_IP_Address: float
+    URL_Length: float
+    Shortining_Service: float
+    having_At_Symbol: float
+    double_slash_redirecting: float
+    Prefix_Suffix: float
+    having_Sub_Domain: float
+    SSLfinal_State: float
+    Domain_registeration_length: float
+    Favicon: float
+    port: float
+    HTTPS_token: float
+    Request_URL: float
+    URL_of_Anchor: float
+    Links_in_tags: float
+    SFH: float
+    Submitting_to_email: float
+    Abnormal_URL: float
+    Redirect: float
+    on_mouseover: float
+    RightClick: float
+    popUpWidnow: float
+    Iframe: float
+    age_of_domain: float
+    DNSRecord: float
+    web_traffic: float
+    Page_Rank: float
+    Google_Index: float
+    Links_pointing_to_page: float
+    Statistical_report: float
